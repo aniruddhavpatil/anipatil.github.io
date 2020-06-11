ea98aaea426b8fa5fa9d981a6da22960026b602b
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-06-11 17:03:53 -0400

    Changed title and h1

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Aniruddha Patil</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to my website!
         </h1>
 
         <p className="description">
